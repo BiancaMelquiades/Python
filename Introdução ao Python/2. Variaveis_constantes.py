bd5deed9519c473e31a740457061f7423111838d
BiancaMelquiades <biancamelquiadesbia@gmail.com>
2023-10-06 22:51:27 -0300

    Fundamentos Python

---
+++ Introdução ao Python/2. Variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Bianca"
+idade = 20
+
+nome = "Bianca alterado"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
---
+++ Introdução ao Python/4. print_input.py
@@ -0,0 +1,7 @@
+nome = input("Digite o seu nome: ")
+idade = input("Digite sua idade: ")
+
+print(nome, idade)
+print("teste", end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
