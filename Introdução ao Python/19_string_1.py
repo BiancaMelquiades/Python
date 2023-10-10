0f76559e9f018c65dde64b2347c965c16eba0573
BiancaMelquiades <biancamelquiadesbia@gmail.com>
2023-10-10 11:45:41 -0300

    strings parte 1

---
+++ Introdução ao Python/19_string_1.py
@@ -0,0 +1,36 @@
+nome = "gUIlerMe"
+
+# Formatação de palavras
+# Todas as letras Maíúsculas
+print(nome.upper())
+# Todas as letras Minúsculas
+print(nome.lower())
+# Primeira maiúcula e demais minúscula
+print(nome.title())
+
+texto = "  Olá mundo!  "
+
+# remove espaços em br
+print(texto + ".")
+
+# remove espaços das laterais
+print(texto.strip() + ".")
+
+# remove espaços da esquerda
+print(texto.rstrip() + ".")
+
+# remove espaços da direita
+print(texto.lstrip() + ".")
+
+menu = "Python"
+
+print("####" + menu + "####")
+
+# Centraliza a string de acordo com o número definido
+print(menu.center(14))
+
+# Concatena palavras para atingir um determinado número de caracteres
+print(menu.center(14, "#"))
+
+# insere o caracter desejado intercalando a cada caracter da string
+print("-".join(menu))
