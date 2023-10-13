e3b3a8992d0db6d4b23e561634ceefa505dfd9de
BiancaMelquiades <biancamelquiadesbia@gmail.com>
2023-10-13 15:39:16 -0300

    estudando funções

--- 01 - Estrutura de dados/05 - Funções/04_parametros_somente_por_posicao copy.py
+++ 01 - Estrutura de dados/05 - Funções/04_parametros_somente_por_posicao copy.py
@@ -1,6 +1,10 @@
+# def f(pos1, pos2, /, pos_or_kwd, *, kwd1, kwd2):
+# def f(positional only /positional or keyword, *, keyword only):
+
 def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 
-
+# válido: os elementos de antes da barra (/) são posicionais
 criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
+# inválido: os elementos de antes da barra (/) não são posicionais
 criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
